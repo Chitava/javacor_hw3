4f7fafda166a95a2619e0940cc13f3d7d61a1dc3
unknown <chitava@mail.ru>
2023-07-21 19:48:13 +0300

    Написать метод выводящую всю доступную информацию об объекте с использованием форматирования строк.

---
+++ untitled/src/main/java/main.java
@@ -0,0 +1,9 @@
+import workers.Worker;
+
+public class main {
+    public static void main(String[] args) {
+        Worker worker = new Worker("Игорь", "вахтер", 89263, 1500,29);
+        worker.printinfo();
+
+    }
+}
--- untitled/src/main/java/workers/Worker.java
+++ untitled/src/main/java/workers/Worker.java
@@ -6,4 +6,39 @@ public class Worker {
     public int phone;
     public float salary;
     public int age;
+
+    public Worker(String name, String position, int phone, float salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void printinfo() {
+        System.out.printf("Сотрудник - " + getName() + ", возраст - " + getAge() + ", должность - " + getPosition() +
+                ", с зарплатой - " + getSalary() + ", телефон - " + getPhone());;
+    }
 }
